1e8aa24e9f0cc79585136dd94a90fade38e43e73
iliyalesani <iliyalesani@gmail.com>
2021-01-31 11:26:57 +0330

    Create 14 - Scope.cs

---
+++ 14 - Scope.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(Array elements)
+    {
+        this.elements = (int[])elements;
+    }
+    internal void computeDifference()
+    {
+        maximumDifference = elements.Max() - elements.Min();
+    }
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
